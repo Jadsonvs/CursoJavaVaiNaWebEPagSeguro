d56ba91fb67aef7e970de4016752285a5cfb6e5d
Jadson Viana <86702743+Jadsonvs@users.noreply.github.com>
2022-09-20 10:46:42 -0300

    Delete facility directory

--- facility/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"facility.emoction"},{"l":"facility.emoction.main"}];updateSearchResults();
--- facility/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"facility.emoction","l":"Emoction"},{"p":"facility.emoction.main","l":"Main"}];updateSearchResults();
